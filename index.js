d2b96619cbbef91cbcefbcd01da2223334d9e9b4
cjsmagic <clarencemerchant1995@gmail.com>
2021-09-30 16:43:37 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+const myTransducer = Transducer([(item) => item + 1, (item) => item * 5]);
 
+function Transducer(functions) {
+  return function (item) {
+    let result = null;
 
+    for (const f in functions) {
+      result = f === 0 ? functions[f](item) : functions[f](result);
+      console.log(result);
+    }
 
-const myTransducer = Transducer([
-  item=>item+1,
-  item=>item*5,
-]);
-
-
-function Transducer(pureFunctions){
-
-  
-  
-  
-
+    return result;
+  };
 }
+
+console.log([1, 2, 3, 4, 5].map(myTransducer));
